4da85e092c4840d9ac4082823b4582ffc7e43278
mlombard82 <mlombard82@gmail.com>
2017-01-17 07:21:22 -0500

    make an authenticated request

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const user = require('../models/user');
+const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
